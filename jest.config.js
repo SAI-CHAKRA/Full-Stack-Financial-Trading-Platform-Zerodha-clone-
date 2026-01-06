7bfa2e1c9f273cd8de45eb5f7783e7f60587f116
SAI-CHAKRA <116057005+SAI-CHAKRA@users.noreply.github.com>
2026-01-06 18:05:12 +0530

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-    // jest.config.js
-    export default {
-      testEnvironment: 'jsdom',
-      setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-      transform: {
-        '^.+\\.jsx?$': 'babel-jest', // Use babel-jest for JavaScript files
-      },
-    };
